21e64f1766bc2ccfa1d5ec2f7fcc47cf6500e1cc
hassan12361 <1425hzah@gmail.com>
2024-09-15 16:36:56 +0300

    student list

--- Lab3.py
+++ Lab3.py
@@ -1,3 +1,10 @@
-def hi():
-    print(2)
-print(1)
+import random 
+
+def studentList():
+    print("hi")
+    list = []
+    for i in range(10):
+        id = random.randint(1,10**9)
+        list.append(id)
+    return list
+print(studentList())
