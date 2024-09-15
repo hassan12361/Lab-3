90b68941c05c82ef50116cf30a6df9a87debdc1c
hassan12361 <1425hzah@gmail.com>
2024-09-15 16:50:32 +0300

    random groub

--- Lab3.py
+++ Lab3.py
@@ -1,10 +1,22 @@
 import random 
 
 def studentList():
-    print("hi")
+    
     list = []
     for i in range(10):
         id = random.randint(1,10**9)
         list.append(id)
     return list
-print(studentList())
+def randomGroup(list):
+    groupList = []
+    i = 0 
+    while(i < len(list)):
+        
+        groupList.append([list[i],list[i+1]])
+        i = 2+i
+    return groupList
+
+
+list = studentList()
+print(list)
+print(randomGroup(list))
